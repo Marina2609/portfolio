e2074e4772cf65805dc3e5bf2b7d7108a547768a
Marina2609 <78434449+Marina2609@users.noreply.github.com>
2026-05-17 22:54:04 +0300

    refactor: code refactoring

--- src/modules/App.js
+++ src/modules/App.js
@@ -212,7 +212,8 @@ export default class App {
 
   playSound(resultName) {
     const audio = new Audio(`./assets/mp3/${resultName}.mp3`);
-    audio.volume = this.settings.config.volume / 100;
+    // audio.volume = this.settings.config.volume / 100;
+    audio.volume = this.settings.config.volume / 150;
     audio.currentTime = 0;
     audio.play().catch(() => console.warn("Звуковой эффект не найден"));
   }
